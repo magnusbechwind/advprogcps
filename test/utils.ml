88e11135fb499715c040477d17e11fd8216122ee
LoZander <lozander@proton.me>
2025-11-05 02:29:46 +0100

    Tests for parser and lambda eval.

---
+++ test/utils.ml
@@ -0,0 +1,16 @@
+open Lib
+
+let initLexer filename =
+  let input = open_in ("../../../../../test/test_programs/" ^ filename) in
+  let filebuf = Lexing.from_channel input in
+  filebuf.lex_curr_p <- { filebuf.lex_curr_p with pos_fname = filename };
+  (input, filebuf)
+  
+let parse file =
+  let input, filebuf = initLexer file in
+  let lexRes =
+      Parser.prog Lexer.token filebuf
+    in
+  close_in input;
+  lexRes
+
