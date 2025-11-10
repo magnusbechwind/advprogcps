7298e63bcde116cf3e43f32cb12679cd2dfdf5b6
LoZander <lozander@proton.me>
2025-11-10 02:05:20 +0100

    Added transform_tests and added `interp` to `Evalcps`.
    `Interp` is an outermost wrapper for CPS evaluation.
    It evaluates a program in the empty context and returns
    the programs return value if it's an int (otherwise it is not
    a well formed program output).

--- test/utils.ml
+++ test/utils.ml
@@ -1,3 +1,11 @@
+open Base
 open Lib
 
+let comparisonTest filename =
+  let lambda_prog = filename |> Fileparser.parse |> Stdlib.Option.get in
+  let cps_prog = Transform.cps lambda_prog in
 
+  let lambda_res = Interp.interp lambda_prog in
+  let cps_res = Evalcps.interp cps_prog in
+
+  [%test_eq: int option] lambda_res cps_res
