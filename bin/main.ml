f70e8a1cd5ba2b4d2448fb459b4aca784cbbdafe
Magnus Bech Wind <202107448@post.au.dk>
2025-11-11 15:01:54 +0100

    implemented rudimentary data gathering and printing to console

--- bin/main.ml
+++ bin/main.ml
@@ -18,7 +18,7 @@ let args = Sys.argv
 
 let () = 
   let x = parse (match Array.length args with
-  | 1 -> "test.lambda"
+  | 1 -> "line.lambda"
   | _ -> (Array.get args 1) ^ ".lambda") in
   match x with
     Some e ->
@@ -27,6 +27,14 @@ let () =
     let cp = Transform.cps e in
     Printf.printf "CPS:\n";
     PrintBox_text.output stdout (Prettycps.cps_to_tree cp);
+
+    let comp = Data.complexity cp in
+    Printf.printf "\nComplexity of CPS :\n";
+    PrintBox_text.output stdout (Data.comp_to_tree comp);
+    Printf.printf "\nComplexity of CPS with details:\n";
+    PrintBox_text.output stdout (Data.comp_to_tree ~show_details:true comp);
+
+
     Printf.printf "\nCPS AST:\n%s\n" (Prettycps.cps_ast_repr cp);
     Printf.printf "Result: %s\n" (match Evalcps.eval [] cp [] with
     | Evalcps.Bool b -> string_of_bool b
