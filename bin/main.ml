28c3c5f19e9b25c88a5db8b4c0b4e8afc0176c4f
LoZander <lozander@proton.me>
2025-12-02 06:32:18 +0100

    cli stuff

--- bin/main.ml
+++ bin/main.ml
@@ -2,52 +2,96 @@ open Lib
 
 let args = Sys.argv
 
-let () = 
-  let x = Fileparser.parse (match Array.length args with
-  | 1 -> "test.lambda"
-  | _ -> (Array.get args 1) ^ ".lambda") in
-  match x with
-    Some e ->
-    Printf.printf "AST:\n";
-    PrintBox_text.output stdout (Pretty.program_to_tree e); print_endline "\n";
+let usage_msg = "main [-betac] [-cfold] <file>"
+
+let betac = ref false
+let cfold = ref false
+let lam = ref false
+let ast = ref false
+let cps = ref false
+let raw = ref false
+let comp = ref false
+let eval = ref false
+let input_file = ref None
+
+let file_handle file = input_file := Some file
+
+let speclist = [
+  ("--betac", Arg.Set betac, "Adds beta contraction optimization");
+  ("--cfold", Arg.Set cfold, "Adds constant folding optimization");
+  ("--lam", Arg.Set lam, "Prints lambda code");
+  ("--ast", Arg.Set ast, "Prints lambda AST");
+  ("--cps", Arg.Set cps, "Prints CPS AST");
+  ("--raw", Arg.Set raw, "Prints CPS AST before optimizations");
+  ("--comp", Arg.Set comp, "Prints complexities");
+  ("--eval", Arg.Set eval, "Evaluates program");
+]
+
+let print_ast ast =
+  print_endline "AST:";
+  PrintBox_text.output stdout (Pretty.program_to_tree ast); print_endline "\n"
+
+let print_raw cps =
+  print_endline "RAW:";
+  PrintBox_text.output stdout (Prettycps.cps_to_tree cps); print_endline "\n"
+
+let print_cps cps =
+  print_endline "(optimized) CPS:";
+  PrintBox_text.output stdout (Prettycps.cps_to_tree cps); print_endline "\n"
+
+let () =
+  Arg.parse speclist file_handle usage_msg;
+  let beta = if !betac then Some Beta.beta_contract else None in
+  let fold = if !cfold then Some Constfold.const_fold else None in
+  let trans = [
+    beta;
+    fold;
+  ] |> List.filter_map Fun.id in
+
+  let file = Option.value !input_file ~default:"test.lambda" in
+
+  let lambda = Fileparser.parse file in
+  lambda |> Option.iter(fun e ->
+    if !ast then print_ast e;
+
     let cp = Transform.cps e in
-    Printf.printf "CPS:\n";
-    PrintBox_text.output stdout (Prettycps.cps_to_tree cp); print_endline "\n";
 
-    (* let bd = Beta.beta_contract cp in
-    Printf.printf "BETA DEAD FIX FIXPOINT:\n";
-    PrintBox_text.output stdout (Prettycps.cps_to_tree bd); *)
+    if !raw then print_raw cp;
 
-    (* let comp = Data.complexity cp in
-    Printf.printf "\nComplexity of CPS :\n";
-    PrintBox_text.output stdout (Data.comp_to_tree comp);
-    Printf.printf "\nComplexity of CPS with details:\n";
-    PrintBox_text.output stdout (Data.comp_to_tree ~show_details:true comp); *)
+    let optimized = Optim.fix trans cp in
 
-    let constfold = Optim.fix [Beta.beta_contract; Constfold.const_fold] cp in
-    Printf.printf "BETA + FOLD fixpoint:\n";
-    PrintBox_text.output stdout (Prettycps.cps_to_tree constfold); print_endline "\n";
+    if !cps then print_cps optimized;
 
-    let comp = Data.complexity cp in
-    Printf.printf "\nComplexity of CPS :\n";
-    PrintBox_text.output stdout (Data.comp_to_tree comp);
-    Printf.printf "\nComplexity of CPS with details:\n";
-    PrintBox_text.output stdout (Data.comp_to_tree ~show_details:true comp);
-    let comp2 = Data.complexity constfold in
-    Printf.printf "\nComplexity of BETA + FOLD :\n";
-    PrintBox_text.output stdout (Data.comp_to_tree comp2);
-    Printf.printf "\nComplexity of BETA + FOLD with details:\n";
-    PrintBox_text.output stdout (Data.comp_to_tree ~show_details:true comp2);
+    if !comp then (
+      if !raw then (
+        let c = Data.complexity cp in
+        print_endline "Complexity of raw CPS :";
+        PrintBox_text.output stdout (Data.comp_to_tree c);
+        print_endline "\n";
+      );
+      (* Printf.printf "\nComplexity of CPS with details:\n";
+      PrintBox_text.output stdout (Data.comp_to_tree ~show_details:true comp); *)
+      if !cps then (
+        let c2 = Data.complexity optimized in
+        print_endline "Complexity (optimized) CPS :";
+        PrintBox_text.output stdout (Data.comp_to_tree c2);
+        print_endline "\n";
+      );
+      (* Printf.printf "\nComplexity of BETA + FOLD with details:\n";
+      PrintBox_text.output stdout (Data.comp_to_tree ~show_details:true comp2); *)
+    );
 
-    Printf.printf "\nCPS AST:\n%s\n" (Prettycps.cps_ast_repr cp);
-    let rec evaldval = function
-    | Evalcps.Bool b -> string_of_bool b
-    | Evalcps.Int i -> string_of_int i
-    | Evalcps.Fun (_,Some str) -> (Printf.sprintf "CPS evaluated to a function but we don't want that :(; value was %s" str)
-    | Evalcps.String str -> str
-    | Evalcps.Tuple (tpl,i) -> List.fold_left (fun acc x -> acc ^ ", " ^ evaldval x  ) "" tpl
-    | _ -> "Not a value"
-    in
-    Printf.printf "Result: %s\n" (evaldval (Evalcps.eval [] cp []));
-    ()
-  | _ -> ()
+    (* Printf.printf "\nCPS AST:\n%s\n" (Prettycps.cps_ast_repr cp); *)
+
+    if !eval then (
+      let rec evaldval = function
+      | Evalcps.Bool b -> string_of_bool b
+      | Evalcps.Int i -> string_of_int i
+      | Evalcps.Fun (_,Some str) -> (Printf.sprintf "CPS evaluated to a function but we don't want that :(; value was %s" str)
+      | Evalcps.String str -> str
+      | Evalcps.Tuple (tpl,i) -> List.fold_left (fun acc x -> acc ^ ", " ^ evaldval x  ) "" tpl
+      | _ -> "Not a value"
+      in
+      Printf.printf "Result: %s\n" (evaldval (Evalcps.eval [] cp []));
+    );
+  )
